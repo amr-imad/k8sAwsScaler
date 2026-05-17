53c0cc0614895ad1e7fd0111a0a383fb8bdbf018
Amr Imad <amrimadkh@gmail.com>
2026-05-17 03:24:48 +0300

    adding a simple server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <!DOCTYPE html>
     <html lang="en">
